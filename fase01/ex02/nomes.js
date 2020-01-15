c90280bf6122acc34433fe93043ea051cdae878f
vinicius361 <59851198+vinicius361@users.noreply.github.com>
2020-01-15 20:01:32 -0300

    vinimoraes

--- fase01/ex02/nomes.js
+++ fase01/ex02/nomes.js
@@ -3,14 +3,18 @@ function mostraNome(){
 }
 
 
-
 function nomeDeVolta(){
-console.log("O android paranoide");
+    console.log("O android paranoide");
 }
 
 
-function nomes(nome1, nome2){
-    console.log(nomes("Athur", "Zaphod"));
+function nomes (nome1, nome2){
+    console.log(nome1e2("Athur", "Zaphod"));
+}
+
+
+function nomes (nome1, nome2){
+    console.log(nomes1e2("Marvin", "Cachlote"));
 }
 
 
