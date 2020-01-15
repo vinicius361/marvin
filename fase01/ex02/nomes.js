b204beba4ea2beac5ee803a1fb1125d6cd6bab33
vinicius361 <59851198+vinicius361@users.noreply.github.com>
2020-01-15 19:58:21 -0300

    Silvaaaa

---
+++ fase01/ex02/nomes.js
@@ -0,0 +1,17 @@
+function mostraNome(){
+    console.log ("Marvin");
+}
+
+
+
+function nomeDeVolta(){
+console.log("O android paranoide");
+}
+
+
+function nomes(nome1, nome2){
+    console.log(nomes("Athur", "Zaphod"));
+}
+
+
+
