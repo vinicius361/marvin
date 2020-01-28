de0f74f7c9fa84126a17438076ce8e98cc48244b
vinicius361 <59851198+vinicius361@users.noreply.github.com>
2020-01-27 21:17:34 -0300

    tomaaaa

---
+++ fase03/ex03/vogalOuConsoante.js
@@ -0,0 +1,13 @@
+function vogalOuConsoante(letra){
+    letra= letra.toLowerCase();
+    
+    if (letra=='a' || letra=='e' || letra=='i' || letra=='o'|| letra=='u')
+    {
+        return("Vogal");
+        
+    }else{
+        
+        return("Consoante");
+    }
+}
+
