3e28cb3404f82301131e64354db7f76189ee03d1
vinicius361 <59851198+vinicius361@users.noreply.github.com>
2020-01-29 20:59:38 -0300

    Nike

---
+++ fase03/ex05/acharNumero.js
@@ -0,0 +1,11 @@
+function acharNumero(n1)   {
+    
+    if(n1 <5)
+    return "mercurio";
+    else if(n1 <=10 && n1 >=5)
+    return "venus";
+    else if(n1 <=100 && n1 >10)
+    return "terra";
+    else 
+    return "marte";
+}
