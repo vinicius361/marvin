afc0f977d6a9883296ae3f86a2ae4afd826b9956
vinicius361 <59851198+vinicius361@users.noreply.github.com>
2020-01-29 19:28:16 -0300

    Bone

--- fase03/ex01/nove.js
+++ fase03/ex01/nove.js
@@ -1,10 +1,9 @@
-function nove(num){
-    if (num%2===1)
-    {
-        return("false");
-        
-    }else{
-        
-        return("true");
-    }
+function nove(numero)   {
+    var n1 = numero.toString();
+    var n2 = n1.substring(n1.length-1,n1.length);
+    
+    if (n2==="9")
+    return true
+    else 
+    return false
 }
