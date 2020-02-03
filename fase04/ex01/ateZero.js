b13d80752267f02dabe9f6358add13fbd5f57a24
vinicius361 <59851198+vinicius361@users.noreply.github.com>
2020-02-03 19:09:19 -0300

    Comida gostosa

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,17 @@
+function ateZero(num){
+    var zero = 0;
+    if(num > 0){
+    while(zero <= num){
+        console.log(zero)
+        zero++;
+    }
+    }
+    else{
+    while(num<=0){
+        console.log(num)
+        num++;
+    }
+}
+}
+
+   
